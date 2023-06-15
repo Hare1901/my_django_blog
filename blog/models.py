2a77c5a776ee426923f9a2c4a100f6aae51eeeb6
hare1901 <hare1901@yandex.ru>
2023-06-15 19:50:12 +0300

    Определили перечисляемый класс Status, добавили параметр статус в Post, регистрация приложения в админке.

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,33 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 #представление поста в блоге
 class Post(models.Model):
 
+    class Status(models.TextChoices):
+        DRAFT = "DF", "DRAFT"
+        PUBLISHED = "PB", "Published"
+
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="blog_posts"
+    )
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=250,
+        choices=Status.choices,
+        default=Status.DRAFT
+
+    )
+
 
     class Meta:
         # сортировка по убыванию
