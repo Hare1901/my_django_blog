a3648f9514b2a17d7d15bbcdfa04990d1d9aaab1
hare1901 <hare1901@yandex.ru>
2023-06-16 06:40:24 +0300

    Адаптация внешнего вида админки приложения Post

--- blog/admin.py
+++ blog/admin.py
@@ -3,5 +3,14 @@ from .models import Post
 
 
 
-admin.site.regoster(Post)
-
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'publish', 'author']
+    list_filter = ['status', 'created', 'publish', 'author']
+    search_fields = ['author', 'title']
+    # автоматическое заполнение из поля title
+    prepopulated_fields = {'slug': ('title',)}
+    # теперь при заполнении выбираешь из выпадающегно списка
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
