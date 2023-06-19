c841678fb8595369d942d977a9b6ee8e51d4843c
Hare1901 <hare1901@yandex.ru>
2023-06-19 18:54:43 +0300

    Разработка представлений списка и детальной информации

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import Http404
 
-# Create your views here.
+from .models import Post
+
+
+def post_list(request):
+
+    # published - созданный нами менеджер
+    posts = Post.published.all()
+    return render(
+        request,
+        'blog/post/list.html',
+        {'posts': posts}
+    )
+
+
+# Пытаемся извлечь объект пост, если возникает ошибка 404 сообщаем что постов не существует
+def post_detail(request, id):
+
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404('No Post Found')
+
+    return render(
+        request,
+        'blog/post/detail.html',
+        {'post': post}
+    )
