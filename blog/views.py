44e374383d77ffa02f023a34caebd1cd9d7988c9
Hare1901 <hare1901@yandex.ru>
2023-06-27 05:46:05 +0300

    Разработка представлений на основе классов

--- blog/views.py
+++ blog/views.py
@@ -1,32 +1,40 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 from .models import Post
 
+class PostListView(ListView):
 
-def post_list(request):
+    #указываем конкретный набор qweryset, по умолчанию Post.object.all()
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
-    # published - созданный нами менеджер
-    post_list = Post.published.all()
-    # Постраничная разбивка по 3 поста на страницу
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-
-    except PageNotAnInteger:
-        # если page number дробное, то выводит первую страницу
-        posts=paginator.page(1)
-
-    except EmptyPage:
-        # Если page number имеет недопустимое значение, выдает последнюю страницу разбивки
-        posts=paginator.page(paginator.num_pages)
-
-    return render(
-        request,
-        'blog/post/list.html',
-        {'posts': posts}
-    )
+# def post_list(request):
+#
+#     # published - созданный нами менеджер
+#     post_list = Post.published.all()
+#     # Постраничная разбивка по 3 поста на страницу
+#     paginator = Paginator(post_list, 3)
+#     page_number = request.GET.get('page', 1)
+#     try:
+#         posts = paginator.page(page_number)
+#
+#     except PageNotAnInteger:
+#         # если page number дробное, то выводит первую страницу
+#         posts=paginator.page(1)
+#
+#     except EmptyPage:
+#         # Если page number имеет недопустимое значение, выдает последнюю страницу разбивки
+#         posts=paginator.page(paginator.num_pages)
+#
+#     return render(
+#         request,
+#         'blog/post/list.html',
+#         {'posts': posts}
+#     )
 
 
 # Пытаемся извлечь объект пост, если возникает ошибка 404 сообщаем что постов не существует
--- my_django_blog/urls.py
+++ my_django_blog/urls.py
@@ -2,10 +2,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from blog.views import post_list
+from blog.views import PostListView
 
 urlpatterns = [
-    path('', post_list, name='post_list'),
+    path('', PostListView.as_view(), name='post_list'),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
 ]
