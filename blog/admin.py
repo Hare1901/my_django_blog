feab3e1ade2dcb872405a5025040b58c83d57a85
Hare1901 <hare1901@yandex.ru>
2023-07-02 19:52:07 +0300

    Создание системы комментариев(добавление в админку)

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Post
+
+from .models import Post, Comment
 
 
 
@@ -15,3 +16,9 @@ class PostAdmin(admin.ModelAdmin):
     #
     date_hierarchy = 'publish'
     ordering = ['status', 'publish']
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'body', 'created', 'active']
+    list_filter = ['active', 'created', 'updated']
+    search_fields = ['name', 'email', 'body']
