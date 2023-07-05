7aaf6c18d59da75302d8c00ef0a70d1e07efc0ee
Hare1901 <hare1901@yandex.ru>
2023-07-05 18:30:35 +0300

    Извлечение постов по сходству(тегам)

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.core.paginator import PageNotAnInteger, Paginator, EmptyPage
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.views.decorators.http import require_POST
+from django.db.models import Count
 
 from taggit.models import Tag
 
@@ -55,14 +56,26 @@ def post_detail(request, year, month, day, post):
 
     # формируем список активных комментариев
     comments = post.comments.filter(active=True)
+
     # форма для комментирования пользователем
     form = CommentFrom()
+
+    # Список постов по одинаковым тегам
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(
+        tags__in=post_tags_ids
+    ).exclude(id=post.id)
+    similar_post = similar_posts.annotate(same_tags=Count('tags'))\
+                       .order_by('-same_tags', '-publish')[:4]
+
+
     return render(
         request,
         'blog/post/detail.html',
         {'post': post,
          'comments': comments,
-         'form': form
+         'form': form,
+         'similar_posts': similar_posts
          }
     )
 
